77f8156ef04e273b015154b22fc98ce3ba018f0a
usuario169 <mijailpalomino@upeu.edu.pe>
2020-11-10 23:35:56 -0500

    v2_2

--- src/app/components/Asociacion/update-asociacion/update-asociacion.component.ts
+++ src/app/components/Asociacion/update-asociacion/update-asociacion.component.ts
@@ -11,56 +11,57 @@ import swal from 'sweetalert2';
 })
 export class UpdateAsociacionComponent implements OnInit {
 
-  asociaciones: any;  
-  asociacion:Asociacion=new Asociacion();
-  constructor(private asociacionService:AsociacionService,
-    private router: Router, private activatedRoute:ActivatedRoute) { }
+  asociaciones: any;
+  asociacion: Asociacion = new Asociacion();
+  constructor(private asociacionService: AsociacionService,
+    private router: Router, private activatedRoute: ActivatedRoute) { }
   ngOnInit(): void {
     this.cargarAsociacion();
   }
-cargarAsociacion():void{
-  this.activatedRoute.params.subscribe(params=>{
-    let id = params['id']
-    if(id){
-      this.asociacionService.getAsociacion(id).subscribe(
-        (data)=>{
-        this.asociaciones=data['cursor_asociacion'] 
-        this.asociacion.NOMBRE_ASOCIACION=this.asociaciones[0].NOMBRE_ASOCIACION;
-        this.asociacion.ID_ASOCIACION=this.asociaciones[0].ID_ASOCIACION;
-        this.asociacion.NUMERO_MIENBROS=this.asociaciones[0].NUMERO_MIENBROS;
-        this.asociacion.FECHA_CONSTITUCION=this.asociaciones[0].FECHA_CONSTITUCION;
-        this.asociacion.ID_SOLICITANTE=this.asociaciones[0].ID_SOLICITANTE;
-        this.asociacion.IMFORMACION_ADICIONAL=this.asociaciones[0].IMFORMACION_ADICIONAL;
-        this.asociacion.UBICACION_ID_UBI_HIJO=this.asociaciones[0].UBICACION_ID_UBI_HIJO;
-        this.asociacion.JUNTA_VECINAL_ID_JD=this.asociaciones[0].ID_JUNTA_VECINAL_ID_JDOL;
-        this.asociacion.DENOMINACION_ID_DENOMINACION=this.asociaciones[0].DENOMINACION_ID_DENOMINACION;
+  cargarAsociacion(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if (id) {
+        this.asociacionService.getAsociacion(id).subscribe(
+          (data) => {
+            this.asociaciones = data['cursor_asociacion']
+            this.asociacion.ID_ASOCIACION = this.asociaciones[0].ID_ASOCIACION;
+            this.asociacion.NOMBRE_ASOCIACION = this.asociaciones[0].NOMBRE_ASOCIACION;
+            this.asociacion.IMFORMACION_ADICIONAL = this.asociaciones[0].IMFORMACION_ADICIONAL;
+            this.asociacion.DENOMINACION_ID_DENOMINACION = this.asociaciones[0].DENOMINACION_ID_DENOMINACION;
+            this.asociacion.NUMERO_MIENBROS = this.asociaciones[0].NUMERO_MIENBROS;
+            this.asociacion.UBICACION_ID_UBI_HIJO = this.asociaciones[0].UBICACION_ID_UBI_HIJO;
+            this.asociacion.FECHA_CONSTITUCION = this.asociaciones[0].FECHA_CONSTITUCION;
+            this.asociacion.JUNTA_VECINAL_ID_JD = this.asociaciones[0].ID_JUNTA_VECINAL_ID_JDOL;
+            this.asociacion.ID_SOLICITANTE = this.asociaciones[0].ID_SOLICITANTE;
 
+
+          })
+      }
+    })
+  }
+  modificarAsociacion(): void {
+    this.asociacionService.updateAsociacion(this.asociacion).subscribe(
+      response => {
+        swal.fire({
+          title: 'Estas seguro?',
+          text: "No podras revertir esto!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, update it!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.router.navigate(['/listar'])
+            swal.fire(
+              'Actualizado!',
+              'El registro ha sido Modificado.',
+              'success'
+            )
+          }
+        })
       })
-    }
-  })
-}
-modificarAsociacion():void{
-  this.asociacionService.updateAsociacion(this.asociacion).subscribe(
-    response=>{
-      swal.fire({
-        title: 'Estas seguro?',
-        text: "No podras revertir esto!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, update it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          this.router.navigate(['/listar'])
-          swal.fire(
-            'Actualizado!',
-            'El registro ha sido Modificado.',
-            'success'
-          )
-        }
-      })    
-  })
-}
+  }
 
 }
